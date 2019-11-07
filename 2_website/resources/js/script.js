b6692b2b8a123baf32e8e56285026091da564f80
Jokubas126 <jokubas126@users.noreply.github.com>
2019-11-07 21:42:37 +0100

    jQuery script added + Sticky nav
    included script.js in which created the sticky navigation appear, when getting out of the header

---
+++ 2_website/resources/js/script.js
@@ -0,0 +1,16 @@
+/* global $ */
+
+/* eslint-env jquery */
+
+$('document').ready(function() {
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    
+});
